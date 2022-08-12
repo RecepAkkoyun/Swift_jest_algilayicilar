1afd98e660f658f7e032bd357d9ae556db3f4de9
Recep <106531235+RecepAkkoyun@users.noreply.github.com>
2022-08-12 10:53:55 +0300

    jest_algilama
    swift ile nesne üzerine tiklandiginda tepki verme uygulamasi

--- tekrarJestAlgilayici/ViewController.swift
+++ tekrarJestAlgilayici/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblIstanbul: UILabel!
+    @IBOutlet weak var imgIstanbul: UIImageView!
+    
+    var istanbul = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imgIstanbul.isUserInteractionEnabled = true // burda etkileşimi algıladı ama hala etkileşimde ne                                                                                    yapıcağını bilmiyor, hemen tanımlayalım.
+        
+        let etkilesim = UITapGestureRecognizer(target:self, action: #selector(imgDegistir)) // burda etkileşimi üzerine                                                                          tıklandığında olarak tanımladım. şimdi bu ikisini birleştirelim
+        
+        imgIstanbul.addGestureRecognizer(etkilesim) // birlestirme islemini de burda yaptim.
+        
     }
-
-
+    @objc func imgDegistir(){
+        if istanbul == true{
+            imgIstanbul.image = UIImage(named: "ankara")
+            lblIstanbul.text = "Ankara"
+            istanbul = false
+        }
+        else{
+            imgIstanbul.image = UIImage(named: "istanbul")
+            lblIstanbul.text = "Istanbul"
+            istanbul = true
+        }
+        
+    }
+    
 }
 
